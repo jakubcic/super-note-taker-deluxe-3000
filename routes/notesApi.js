80727fdd9be2eab3cd92e13791846ccca5dc85e2
jakub <jakub@uconn.edu>
2022-12-04 17:52:06 -0500

    add /api/notes GET and POST to view notes and add new one

--- routes/notesApi.js
+++ routes/notesApi.js
@@ -4,12 +4,37 @@ const {
   readFromFile,
   readAndAppend,
   writeToFile,
-} = require("./helpers/fsUtils");
+} = require("../helpers/fsUtils");
 
 // GET route to get all notes
+api.get("/notes", (req, res) => {
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
 
 
 // POST route to create new note
+api.post("/notes", (req, res) => {
+  console.log(req.body);
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+    // write to db.json
+    readAndAppend(newNote, "./db/db.json");
+    // construct response
+    const response = {
+      status: 'Successly added note',
+      body: newNote,
+    };
+    res.json(response);
+  } else {
+    res.json('Error adding new note');
+  }
+});
 
 
 // DELETE route to delete note
