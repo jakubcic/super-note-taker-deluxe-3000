eb4cafe7dc08791e8f9b317e7e1feb0e9f5702e7
jakub <jakub@uconn.edu>
2022-12-04 15:03:18 -0500

    set up basic express server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// GET route for homepage
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+// GET route for notes page
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+// listen on PORT
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+});
