384a06c3903b28729c23f8ebcccf8418967dc1e8
jakub <jakub@uconn.edu>
2022-12-04 16:29:37 -0500

    import and use our notesApi routes

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const path = require("path");
+// import our routes from the routes folder
+const api = require("./routes/notesApi");
 
 const PORT = process.env.PORT || 3001;
 
@@ -7,6 +9,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+// use our api routes
+app.use("/api", api);
 
 // GET route for notes page
 app.get("/notes", (req, res) => {
@@ -18,8 +22,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-
 // listen on PORT
 app.listen(PORT, () => {
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+  console.log(`App listening at http://localhost:${PORT} 🚀`);
 });
