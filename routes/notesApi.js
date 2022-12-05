8e9f8bc0ea88963a54c2c9e1e9a061d3df4a266d
jakub <jakub@uconn.edu>
2022-12-04 19:01:48 -0500

    add /api/notes/:id POST to add new note + bonus GET to get note by id

--- routes/notesApi.js
+++ routes/notesApi.js
@@ -1,4 +1,4 @@
-const api = require('express').Router();
+const api = require("express").Router();
 const { v4: uuidv4 } = require("uuid");
 const {
   readFromFile,
@@ -10,7 +10,6 @@ const {
 api.get("/notes", (req, res) => {
   readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
-
 
 // POST route to create new note
 api.post("/notes", (req, res) => {
@@ -27,17 +26,72 @@ api.post("/notes", (req, res) => {
     readAndAppend(newNote, "./db/db.json");
     // construct response
     const response = {
-      status: 'Successly added note',
+      status: "Successly added note",
       body: newNote,
     };
     res.json(response);
   } else {
-    res.json('Error adding new note');
+    res.json("Error adding new note");
   }
 });
 
+// DELETE route to delete note by id
+api.delete("/notes/:id", (req, res) => {
+  const noteId = req.params.id;
+  console.log(noteId);
+  // read from db.json as promise
+  readFromFile("./db/db.json")
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // filter for note matching noteId (I want to display in response)
+      const noteToDelete = json.filter((note) => note.id === noteId);
+      // if noteToDelete is not empty
+      if (noteToDelete.length > 0) {
+        // filter out note matching noteId
+        const notesToKeep = json.filter((note) => note.id !== noteId);
+        // write filtered notes to db.json
+        writeToFile("./db/db.json", notesToKeep);
+        // construct response
+        const response = {
+          status: "Successfully deleted note",
+          deleted: noteToDelete,
+        };
+        res.json(response);
+      } else {
+        // construct response
+        const response = {
+          status: "No note with that ID",
+        };
+        res.json(response);
+      }
+    });
+});
 
-// DELETE route to delete note
+// bonus: GET route to get a specific note by id
+api.get("/notes/:id", (req, res) => {
+  const noteId = req.params.id;
+  console.log(noteId);
+  readFromFile("./db/db.json")
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // find note in array by id
+      const specificNote = json.find((note) => note.id === noteId);
+
+      // construct response if note not found
+      if (!specificNote) {
+        const response = {
+          status: "No note found with that id",
+        };
+        res.json(response);
+      } else {
+        // construct response if note found
+        const response = {
+          status: "Success",
+          body: specificNote,
+        };
+        res.json(response);
+      }
+    });
+});
 
 module.exports = api;
-
